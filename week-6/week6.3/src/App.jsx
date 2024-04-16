74b9b6f5d78aaa6aecec66e590438c06519c9083
umakant3525 <umakant3525@gmail.com>
2024-04-16 18:41:18 +0530

    done eith useState and useeffect reacap

--- week-6/week6.3/src/App.jsx
+++ week-6/week6.3/src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [exchangeData, setExchangeData] = useState({});
+  const [bankData, setBankData] = useState({});
 
+  useEffect(() => {
+    setTimeout(() => {
+      setBankData({ income: 100 });
+    }, 3000);
+  });
+
+  useEffect(() => {
+    setTimeout(() => {
+      setExchangeData({ returns: 100 });
+    }, 1000);
+  });
+
+  const incomeTax = (bankData.income + exchangeData.returns) * 0.3;
   return (
     <>
-   
-
-   {/* Here is your usestate code  */}
+      {/* Here is your usestate code  */}
       <h1> useState </h1>
       <p>here the concepst of rerendering and reconsillation</p>
       <div className="card">
@@ -17,13 +30,10 @@ function App() {
         </button>
       </div>
 
-
-      
-
-
-    
+      {/* Here is the useEffect logic goes  */}
+      <h3>Income tax : {incomeTax}</h3>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
